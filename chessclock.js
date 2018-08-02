085e1dd554288444801784badac291927d3e7d77
bwallace <17948072+iambw@users.noreply.github.com>
2018-08-02 15:24:21 -0400

    Fixed functions to add/subtract time from clock

--- chessclock.js
+++ chessclock.js
@@ -1,55 +1,58 @@
-var sec = 0;
-var min = 10;
-var playing = false;
 
+var sec = 0;
+
+var playing = false;
+var dft = document.getElementById('p1-min');
 var increaseP1Time = document.getElementById('p1-min');
 var decreaseP1Time = document.getElementById('p1-min');
 var increaseP2Time = document.getElementById('p2-min');
 var decreaseP2Time = document.getElementById('p2-min');
 var rstbtn = document.getElementById('rst-btn');
+var srtbtn = document.getElementById('strt-btn');
+
+
 
 // Player buttons
 function increaseP1() {
-  min += 1;
-  console.log(typeof min);
+  min = document.getElementById('p1-min').innerHTML;
+  min = parseInt(min, 10) + 1;
+  if (min < 10) {
+    min = '0' + min;
+  }
   increaseP1Time.innerHTML = min;
 }
 
+
 function decreaseP1() {
-  min -= 1;
-  console.log(typeof min);
+  min = document.getElementById('p1-min').innerHTML;
+  min = parseInt(min, 10) - 1;
+  if (min < 10) {
+    min = '0' + min;
+  }
   decreaseP1Time.innerHTML = min;
 }
 
+
+
 function increaseP2() {
-  min += 1;
+  min = document.getElementById('p2-min').innerHTML;
+  min = parseInt(min, 10) + 1;
+  if (min < 10) {
+    min = '0' + min;
+  }
   increaseP2Time.innerHTML = min;
 }
 
+
 function decreaseP2() {
-  min -= 1;
+  min = document.getElementById('p2-min').innerHTML;
+  min = parseInt(min, 10) - 1;
+  if (min < 10) {
+    min = '0' + min;
+  }
   decreaseP2Time.innerHTML = min;
 }
-// END Player buttons
 
-function countdown() {
-  var p1Time = document.getElementById('p1-min');
-  p1Time -= 1;
-  p1Time.innerHTML = min;
-  if (p1Time == 0) {
-    p1Time.innerHTML = "00";
-    timeOutPlayer();
-  }
-}
-
-
-var timeId = setInterval(countdown, 1000);
-
-function timeOutPlayer() {
-  clearInterval(timeId);
-}
-
-// setTimeout(timeOutPlayer, 3000);
 
 function reset() {
   var p1Time = document.getElementById('p1-min');
@@ -58,19 +61,26 @@ function reset() {
   p2Time.innerHTML = 10;
 }
 
-function start() {
-  playing = true;
-  countdown();
-}
-  
 
+function start() {
+  var min = 10;
+  var p1Time = document.getElementById('p1-min');
+  p1Time.innerHTML - 1;
+}
+
+
+var timeId = setInterval(start, 1000);
+
+
+function timeOutPlayer() {
+  clearInterval(timeId);
+}
+
+/*
 function stop() {
   playing = false;
 }
-  
-
-
-
+*/
 
 function switchPlayer() {
   /*
