3c3e98fd95794faecd8bb484fb8f793032fc00b5
bwallace <17948072+iambw@users.noreply.github.com>
2018-12-20 13:17:34 -0500

    Enabled minute update from modal
    Added functions to each button in the modal window.
    
    Using the modal window plus/minus buttons the time can be updated
    for each player.

--- js/timer.js
+++ js/timer.js
@@ -1,9 +1,8 @@
+"use-strict";
 
 
-// const setTime = () => {
-//   let starttime = prompt("Time:");
-//   console.log(starttime);
-// }
+let min1 = Number(document.getElementById("minutes1").innerHTML);
+let min2 = Number(document.getElementById("minutes2").innerHTML);
 
 
 const padZero = (n) => {
@@ -13,18 +12,13 @@ const padZero = (n) => {
   return n;
 }
 
+
 const openModal = () => {
   document.querySelector(".modal").style.display = "flex";
 }
 
-const closeModal = () => {
-  document.querySelector(".modal").style.display = "none";
-}
 
-let min1 = Number(document.getElementById("minutes1").innerHTML);
-let min2 = Number(document.getElementById("minutes2").innerHTML);
-
-const increaseTime = () => {
+const increaseTimeP1 = () => {
   console.log(min1);
   if (min1 === 99) {
     min1 = 98;
@@ -33,7 +27,8 @@ const increaseTime = () => {
   document.getElementById("minutes1").innerHTML = padZero(min1);
 }
 
-const decreaseTime = () => {
+
+const decreaseTimeP1 = () => {
   if (min1 === 1) {
     min1 = 2;
   }
@@ -41,19 +36,27 @@ const decreaseTime = () => {
   document.getElementById("minutes1").innerHTML = padZero(min1);
 }
 
-// const increaseTime = () => {
-//   console.log(min1);
-//   if (min1 === 99) {
-//     min1 = 98;
-//   }
-//   min1 += 1;
-//   document.getElementById("minutes1").innerHTML = padZero(min1);
-// }
 
-// const decreaseTime = () => {
-//   if (min1 === 1) {
-//     min1 = 2;
-//   }
-//   min1 -= 1;
-//   document.getElementById("minutes1").innerHTML = padZero(min1);
-// }
+const increaseTimeP2 = () => {
+  if (min2 === 99) {
+    min2 = 98;
+  }
+  min2 += 1;
+  document.getElementById("minutes2").innerHTML = padZero(min2);
+}
+
+
+const decreaseTimeP2 = () => {
+  if (min2 === 1) {
+    min2 = 2;
+  }
+  min2 -= 1;
+  document.getElementById("minutes2").innerHTML = padZero(min2);
+}
+
+
+const closeModal = () => {
+  document.querySelector(".min1").innerHTML = padZero(min1);
+  document.querySelector(".min2").innerHTML = padZero(min2);
+  document.querySelector(".modal").style.display = "none";
+}
