d96cc953bf6c1fe56d967f24d3abe32d80ae1640
bwallace <17948072+iambw@users.noreply.github.com>
2018-12-27 11:48:16 -0500

    Require favicon
    The favicon was not showing up in the browser tab. Non <img> tags
    must be required to be added to project.
    
    require("images/favicon.svg"); was added to index.js to resolve
    the problem.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require("./quotes.html");
 require("./links.html");
 require("./about.html");
 require("./audio/beep-06.mp3");
+require("./images/favicon.svg");
 require("./audio/click-button-2-soundbible.com-911295385.mp3");
 require("./audio/time-bomb-short-soundbible.com-1562499525.mp3");
 
